01c71fd9c71a420f794905a2b9b1a534206898f4
Hagen Schink <hagen.schink@gmail.com>
2013-12-09 21:19:23 +0100

    Initial commit

---
+++ src/main/java/org/iti/graph/comparison/result/IModificationDetail.java
@@ -0,0 +1,6 @@
+package org.iti.graph.comparison.result;
+
+public interface IModificationDetail {
+
+	public String getIdentifier();
+}
---
+++ src/main/java/org/iti/graph/comparison/result/Type.java
@@ -0,0 +1,14 @@
+package org.iti.graph.comparison.result;
+
+public enum Type {
+	None,
+
+	NodeAdded,
+	NodeMoved,
+	NodeRenamed,
+	NodeDeleted,
+
+	PathAdded,
+	PathRenamed,
+	PathDeleted,
+}
